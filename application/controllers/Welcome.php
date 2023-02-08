1470e01933ca2ada9a86e489c2f430c24ce0332c
Andrianina <rakotozananya5@gmail.com>
2023-02-08 11:17:49 +0300

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,6 +18,16 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+	public function __construct()
+    {
+        parent::__construct();
+       
+        if(!$this->session->has_userdata('mail'))
+		{
+			redirect('welcome/index');  
+		}
+		$this->load->model('Model');
+    }
 	public function index()
 	{
 		$this->load->view('login');
